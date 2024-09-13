217c71084d855f30161c2a3f03e65898bcd2c11c
coolbeatz71 <sigmacool@gmail.com>
2024-09-13 22:05:48 +0200

    [chore] use react-icon on the toggler button icon

--- src/components/layout/navigation/Navigation.tsx
+++ src/components/layout/navigation/Navigation.tsx
@@ -72,7 +72,7 @@ export const Navigation = (): JSX.Element => {
                                     href="#"
                                     className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                                 >
-                                    Contributions
+                                    Contribution
                                 </a>
                                 <a
                                     href="#"
@@ -84,7 +84,7 @@ export const Navigation = (): JSX.Element => {
                                     href="#"
                                     className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                                 >
-                                    Skills
+                                    Skill
                                 </a>
                                 <a
                                     href="#"
