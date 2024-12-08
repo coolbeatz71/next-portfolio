34ddf9e684bb41ea7da5873cda1cbaf9199360a7
coolbeatz71 <sigmacool@gmail.com>
2024-12-08 19:30:02 +0200

    create the tooltip component

--- src/components/shared/theme-toggle/ThemeToggle.tsx
+++ src/components/shared/theme-toggle/ThemeToggle.tsx
@@ -1,20 +1,33 @@
+import Tooltip from "@/components/shared/tooltip/Tooltip";
 import { useTheme } from "next-themes";
+import { useCallback, useMemo } from "react";
 import { FaCloudMoon, FaCloudSun } from "react-icons/fa";
 
 export default function ThemeToggle(): JSX.Element {
     const { theme, setTheme } = useTheme();
 
-    const onToggle = () => {
+    const onToggle = useCallback(() => {
         setTheme(theme === "dark" ? "light" : "dark");
-    };
+    }, [theme, setTheme]);
+
+    const icon = useMemo(
+        () => (theme === "dark" ? <FaCloudSun /> : <FaCloudMoon />),
+        [theme]
+    );
+    const tooltipText = useMemo(
+        () => (theme === "dark" ? "Light Mode" : "Dark Mode"),
+        [theme]
+    );
 
     return (
-        <button
-            type="button"
-            onClick={onToggle}
-            className="text-xl bg-foreground text-background rounded size-12 flex justify-center items-center text-gray-400 hover:bg-gray-700 hover:text-white"
-        >
-            {theme === "dark" ? <FaCloudSun /> : <FaCloudMoon />}
-        </button>
+        <Tooltip text={tooltipText}>
+            <button
+                type="button"
+                onClick={onToggle}
+                className="group relative text-xl bg-foreground text-background rounded size-12 flex justify-center items-center text-gray-400 hover:bg-gray-700 hover:text-white"
+            >
+                {icon}
+            </button>
+        </Tooltip>
     );
 }
