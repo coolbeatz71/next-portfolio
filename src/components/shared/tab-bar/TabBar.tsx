6759cbe39b1691fe4b55dc44f4c0875be18eacde
coolbeatz71 <sigmacool@gmail.com>
2024-12-16 13:33:54 +0200

    TabBar: prevent tabbar instances to share active-tab-index state

--- src/components/shared/tab-bar/TabBar.tsx
+++ src/components/shared/tab-bar/TabBar.tsx
@@ -7,12 +7,14 @@ interface TabBarProps {
     tabs: Tabs[];
     tabBarClassName?: string;
     containerClassName?: string;
+    context: "skills" | "projects";
 }
 
 export default function TabBar({
     tabs,
-    containerClassName,
-    tabBarClassName
+    context,
+    tabBarClassName,
+    containerClassName
 }: TabBarProps) {
     const [activeTabIndex, setActiveTabIndex] = useState(0);
 
@@ -20,11 +22,11 @@ export default function TabBar({
         <div className={cn("w-full", containerClassName)}>
             <div className="relative flex flex-col items-center">
                 <ul
+                    data-tabs="tabs"
                     className={cn(
                         "relative flex flex-wrap px-1.5 py-1.5 list-none bg-white dark:bg-gray-900 rounded-lg gap-1 min-w-[50%]",
                         tabBarClassName
                     )}
-                    data-tabs="tabs"
                 >
                     {tabs.map((tab, idx) => (
                         <li
@@ -42,15 +44,14 @@ export default function TabBar({
                                 }}
                             >
                                 {tab.title}
-
                                 {idx === activeTabIndex && (
                                     <motion.span
                                         className="bg-indigo-700 dark:bg-indigo-500 shadow-lg rounded-lg absolute inset-0 -z-10"
-                                        layoutId="activeSection"
+                                        layoutId={`${context}-active`}
                                         transition={{
+                                            damping: 30,
                                             type: "spring",
-                                            stiffness: 380,
-                                            damping: 30
+                                            stiffness: 380
                                         }}
                                     />
                                 )}
