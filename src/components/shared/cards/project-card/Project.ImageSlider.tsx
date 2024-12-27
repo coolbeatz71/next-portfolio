a6ac2eed92c121520c1d6792ea055a4e0c5a6a80
coolbeatz71 <sigmacool@gmail.com>
2024-12-27 15:36:37 +0200

    [refactor] ProjectImageSlider: avoid variables repetition

--- src/components/shared/cards/project-card/Project.ImageSlider.tsx
+++ src/components/shared/cards/project-card/Project.ImageSlider.tsx
@@ -14,6 +14,7 @@ export default function ProjectImageSlider({
 }: ProjectImageSliderProps): JSX.Element {
     const ref = useRef();
     const { elX: elementX } = useMouse(ref);
+    const imageWithoutPreview = images.slice(1);
     const throttledElementX = useThrottle(elementX, 50);
     const [startX, setStartX] = useState<number | null>(null);
 
@@ -26,18 +27,18 @@ export default function ProjectImageSlider({
     const handleNext = useCallback(() => {
         setSliderState((state) => ({
             ...state,
-            currentIndex: (state.currentIndex + 1) % images.slice(1).length
+            currentIndex: (state.currentIndex + 1) % imageWithoutPreview.length
         }));
-    }, [images]);
+    }, [imageWithoutPreview]);
 
     const handlePrev = useCallback(() => {
         setSliderState((state) => ({
             ...state,
             currentIndex:
-                (state.currentIndex - 1 + images.slice(1).length) %
-                images.slice(1).length
+                (state.currentIndex - 1 + imageWithoutPreview.length) %
+                imageWithoutPreview.length
         }));
-    }, [images]);
+    }, [imageWithoutPreview]);
 
     useInterval(() => handleNext(), isPlaying ? 5000 : null);
 
@@ -60,14 +61,14 @@ export default function ProjectImageSlider({
 
     const slideStyles = useMemo(
         () =>
-            images.slice(1).map((_, index) => {
+            imageWithoutPreview.map((_, index) => {
                 const position =
-                    (index - currentIndex + images.slice(1).length) %
-                    images.slice(1).length;
+                    (index - currentIndex + imageWithoutPreview.length) %
+                    imageWithoutPreview.length;
                 const isActive = position === 0;
-                const baseWidth = 400;
+                const baseWidth = 520;
                 const minWidth = 100;
-                const widthReduction = 75;
+                const widthReduction = 95;
 
                 const width = isActive
                     ? baseWidth
@@ -85,11 +86,11 @@ export default function ProjectImageSlider({
                 return {
                     width,
                     left,
-                    zIndex: images.slice(1).length - position,
+                    zIndex: imageWithoutPreview.length - position,
                     opacity: position < 4 ? 1 : 0
                 };
             }),
-        [currentIndex, images]
+        [currentIndex, imageWithoutPreview]
     );
 
     return (
@@ -100,7 +101,7 @@ export default function ProjectImageSlider({
                 onClick={togglePlayPause}
                 onKeyDown={togglePlayPause}
                 onMouseDown={handleMouseDown}
-                className="relative w-full max-w-6xl px-4 py-16 pr-0 h-[350px] cursor-grab"
+                className="relative w-full max-w-6xl px-4 py-4 pr-0 h-[380px] cursor-grab"
             >
                 <div className="absolute top-0 left-0 w-full h-full z-0">
                     <div className="absolute z-20 w-full h-full bg-slate-200/[0.7] dark:bg-slate-700/[0.7] backdrop-blur-lg rounded-md" />
@@ -108,13 +109,13 @@ export default function ProjectImageSlider({
                         fill
                         src={images[0].src}
                         alt={images[0].alt}
-                        className="object-cover rounded-md"
+                        className="object-cover rounded"
                         loading="lazy"
                     />
                 </div>
                 <div className="w-full h-full relative overflow-hidden">
                     <AnimatePresence initial={false}>
-                        {images.slice(1).map((img, index) => {
+                        {imageWithoutPreview.map((img, index) => {
                             const style = slideStyles[index];
                             if (style.opacity === 0) return null;
                             return (
@@ -126,7 +127,7 @@ export default function ProjectImageSlider({
                                         duration: 0.5,
                                         ease: "easeInOut"
                                     }}
-                                    className="absolute top-0 overflow-hidden shadow-lg h-full"
+                                    className="absolute top-0 overflow-hidden h-full"
                                 >
                                     <NextImage
                                         fill
