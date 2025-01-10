fcb46bb7d64807e4c6bbd8f55a76cb89443b7c69
coolbeatz71 <sigmacool@gmail.com>
2025-01-10 02:09:08 +0200

    AboutMe: update texts

--- src/locales/fr/aboutme.ts
+++ src/locales/fr/aboutme.ts
@@ -4,7 +4,7 @@ export const aboutme = {
     aboutme_subtitle:
         "Je m’investis également dans l’open-source, où je contribue à des projets promouvant le développement moderne.",
     about_myself: "Quelques détails sur moi",
-    about_myself_content: `{{name}} est un ingénieur logiciel senior chevronné avec plus de 7 ans d'expérience pratique couvrant à la fois le développement frontend et backend. Compétent en`,
+    about_myself_content: `{{name}} est un Ingénieur Logiciel Senior Chevronné avec plus de 7 ans d'expérience pratique couvrant à la fois le développement frontend et backend. Compétent en`,
     about_myself_content_2: "doté d'une expertise avancée en",
     about_myself_content_3: `pour le développement mobile. Professionnel dédié à la résolution de problèmes et à l'apprentissage continu, visant à innover et à offrir des résultats remarquables à chaque projets.`,
     about_myself_content_4: `Je suis titulaire d'une licence en Gestion Informatique, qui m'a apporté à la fois une expertise technique et des solides compétences interpersonnelles. Ces atouts m'ont permis de collaborer efficacement avec des équipes diversifiées et des partenaires pour créer des solutions logicielles d’excellence.`,
