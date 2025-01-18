90b74b223352e676c463413c28521caae2975657
coolbeatz71 <sigmacool@gmail.com>
2025-01-18 11:18:05 +0100

    FooterContact: design the contact form

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,17 +1,14 @@
 import Spotlight from "@/components/shared/spotlight/Spotlight";
+import FooterContact from "./Footer.Contact";
 
 export default function Footer(): JSX.Element {
     return (
-        <footer className="bg-slate-100 dark:bg-slate-700">
+        <footer className="bg-slate-100 dark:bg-slate-700 relative bottom-0">
             <div className="mx-auto max-w-7xl">
                 <Spotlight />
-                <div className="grid grid-cols-1 xl:grid-cols-2 items-center py-10">
+                <div className="grid grid-cols-1 xl:grid-cols-2 items-center py-10 gap-12">
                     <div className="flex flex-col items-start xl:px-0 px-8">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Voluptate ducimus itaque laborum quas incidunt
-                        consequuntur saepe velit corrupti, omnis commodi vitae
-                        molestias ea blanditiis perferendis delectus sint
-                        eligendi expedita placeat?
+                        <FooterContact />
                     </div>
                     <div className="flex justify-center relative">
                         Lorem ipsum dolor sit amet consectetur, adipisicing
