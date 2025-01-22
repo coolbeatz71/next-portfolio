c2311795216f25790984666ccc177992c58e6d46
coolbeatz71 <sigmacool@gmail.com>
2025-01-23 01:25:45 +0200

    Experience: improve the timeline linebar color

--- src/components/shared/timeline/Timeline.tsx
+++ src/components/shared/timeline/Timeline.tsx
@@ -55,7 +55,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
                     style={{
                         height: `${height}px`
                     }}
-                    className="absolute md:left-5 left-5 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-slate-300/50 dark:via-neutral-500 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+                    className="absolute md:left-5 left-5 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-slate-400/30 dark:via-slate-600/30 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)]"
                 >
                     <motion.div
                         style={{
