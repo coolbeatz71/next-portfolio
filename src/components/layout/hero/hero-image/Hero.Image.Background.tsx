e0d641121607d5d90a6462e781dc9038efc01d7e
coolbeatz71 <sigmacool@gmail.com>
2025-01-28 11:18:05 +0100

    HeroImageBackground: use svg instead of clip-path or perspective transform

--- src/components/layout/hero/hero-image/Hero.Image.Background.tsx
+++ src/components/layout/hero/hero-image/Hero.Image.Background.tsx
@@ -8,12 +8,35 @@ export function HeroImageBackground({ className }: HeroImageBackgroundProps) {
     return (
         <div
             className={cn(
-                "absolute m-auto bg-gradient-to-br from-indigo-500/30 via-pink-500/60 to-indigo-500 rounded-lg",
+                "absolute m-auto flex items-center justify-center text-indigo-500",
                 className
             )}
-            style={{
-                transform: "perspective(18rem) rotateY(-12deg)"
-            }}
-        />
+        >
+            <svg
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="4 1 12 9"
+            >
+                <defs>
+                    <linearGradient
+                        id="gradient"
+                        gradientTransform="rotate(45)"
+                    >
+                        <stop offset="0%" stopColor="rgba(99, 102, 241, 0.3)" />
+                        <stop
+                            offset="50%"
+                            stopColor="rgba(236, 72, 153, 0.6)"
+                        />
+                        <stop offset="100%" stopColor="rgba(99, 102, 241, 1)" />
+                    </linearGradient>
+                </defs>
+                <path
+                    d="M 14.85 1 Q 15 1 15 1.15 L 15 9.85 Q 15 10 14.85 10 L 5.15 8.85 Q 5 8.85 5 8.7 L 5 3.15 Q 5 3 5.15 3 L 14.85 1"
+                    fill="url(#gradient)"
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                />
+            </svg>
+        </div>
     );
 }
