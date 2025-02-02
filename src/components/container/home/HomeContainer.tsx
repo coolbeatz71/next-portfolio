17d8f78aaefda50f0a086b419ef546b543b0e67c
coolbeatz71 <sigmacool@gmail.com>
2025-02-02 04:28:07 +0200

    SectionHeader: implement mobile responsiveness

--- src/components/container/home/HomeContainer.tsx
+++ src/components/container/home/HomeContainer.tsx
@@ -1,5 +1,6 @@
 import AboutMe from "@/components/layout/about-me/AboutMe";
 import Companies from "@/components/layout/companies/Companies";
+import DownloadResumeFixedButton from "@/components/layout/fixed-buttons/DownloadResume.Fixed.Button";
 import Hero from "@/components/layout/hero/Hero";
 import Navigation from "@/components/layout/navigation/Navigation";
 import Projects from "@/components/layout/projects/Projects";
@@ -24,9 +25,9 @@ export default function HomeContainer(): JSX.Element {
             <Projects />
             <Skills />
 
-            {/* 
             <DownloadResumeFixedButton />
 
+            {/* 
             <Footer /> */}
         </Fragment>
     );
--- src/components/shared/section-header/SectionHeader.tsx
+++ src/components/shared/section-header/SectionHeader.tsx
@@ -11,7 +11,7 @@ export default function SectionHeader({
     return (
         <div className="flex flex-col items-center justify-center pb-6 md:pb-12">
             <DotBackground className="h-20 z-0" />
-            <div className="text-center lg:px-64 z-10">
+            <div className="text-center xl:px-64 z-10">
                 <h2 className="mb-4 text-3xl font-bold !leading-relaxed text-slate-700 dark:text-slate-300">
                     {title}
                 </h2>
