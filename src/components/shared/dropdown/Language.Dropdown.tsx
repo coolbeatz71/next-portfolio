d353bff8627f084c5d9acbb8e71e467c69696c3a
coolbeatz71 <sigmacool@gmail.com>
2025-02-03 14:55:27 +0200

    Navigation: increase buttons sizes and spacing

--- src/components/shared/dropdown/Language.Dropdown.tsx
+++ src/components/shared/dropdown/Language.Dropdown.tsx
@@ -52,7 +52,7 @@ export default function LanguageDropDown({
             <div>
                 <button
                     type="button"
-                    className="rounded-lg flex items-center justify-center p-2.5 sm:p-2.5 md:p-3.5 text-slate-700 bg-slate-100 dark:bg-slate-900 hover:bg-slate-300 text-sm dark:text-slate-100 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                    className="rounded-lg flex items-center justify-center p-3.5 sm:p-2.5 md:p-3.5 text-slate-700 bg-slate-100 dark:bg-slate-900 hover:bg-slate-300 text-sm dark:text-slate-100 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                     onClick={toggleDropdown}
                 >
                     <span className="mr-2 h-5 w-5 flex items-center justify-center flex-shrink-0">
--- src/components/shared/theme-toggle/ThemeToggle.tsx
+++ src/components/shared/theme-toggle/ThemeToggle.tsx
@@ -18,7 +18,7 @@ export default function ThemeToggle(): JSX.Element {
         <button
             type="button"
             onClick={onToggle}
-            className="group relative text-2xl size-10 sm:size-10 md:size-12 bg-foreground text-background rounded-lg flex justify-center items-center dark:text-indigo-500 text-indigo-700 bg-slate-100 dark:bg-slate-900 hover:bg-slate-300 dark:hover:bg-slate-700"
+            className="group relative text-2xl size-12 sm:size-12 md:size-12 bg-foreground text-background rounded-lg flex justify-center items-center dark:text-indigo-500 text-indigo-700 bg-slate-100 dark:bg-slate-900 hover:bg-slate-300 dark:hover:bg-slate-700"
         >
             {icon}
         </button>
