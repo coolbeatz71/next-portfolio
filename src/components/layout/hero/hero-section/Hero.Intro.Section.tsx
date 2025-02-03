677c377b03de28e9a32750aa696b4eb7a41769b3
coolbeatz71 <sigmacool@gmail.com>
2025-02-03 20:15:16 +0200

    replace FlipWord with TypeWriter component

--- src/components/layout/hero/hero-section/Hero.Intro.Section.tsx
+++ src/components/layout/hero/hero-section/Hero.Intro.Section.tsx
@@ -1,8 +1,8 @@
 import { Fragment } from "react";
 
 import GradientShineButton from "@/components/shared/buttons/gradient-shine/GradientShine.Button";
-import FlipWords from "@/components/shared/flip-words/FlipWords";
 import SocialLinks from "@/components/shared/social-links/SocialLinks";
+import TypeWriter from "@/components/shared/type-writer/TypeWriter";
 import { useTranslation } from "react-i18next";
 
 export default function HeroIntroSection(): JSX.Element {
@@ -14,25 +14,25 @@ export default function HeroIntroSection(): JSX.Element {
                 <SocialLinks />
             </div>
             <br />
-            <h1 className="max-w-xl mb-4 font-bold dark:text-white text-black text-center md:text-start">
-                <span className="text-3xl sm:text-3xl lg:text-4xl">
-                    {t("i_am", { name: "Jean-Vincent" })}
-                </span>
-                <br />
-                <div className="text-4xl sm:text-4xl lg:text-5xl !leading-loose">
-                    <FlipWords
+
+            <div className="flex flex-col gap-12 z-10">
+                <h1 className="max-w-xl dark:text-white text-black text-start">
+                    <span className="text-3xl sm:text-3xl lg:text-4xl font-semibold">
+                        {t("i_am", { name: "Jean-Vincent" })}
+                    </span>
+                    <TypeWriter
                         words={[
                             t("software_engineer"),
                             t("frontend_engineer"),
                             t("mobile_engineer")
                         ]}
-                        className="pl-0 text-indigo-700 dark:text-indigo-500 xl:text-5xl"
+                        className="pl-0 font-bold text-indigo-700 dark:text-indigo-500 text-5xl"
                     />
-                </div>
-            </h1>
-            <p className="mb-6 font-semibold lg:mb-8 text-center md:text-start text-sm sm:text-md lg:text-lg text-slate-600 dark:text-slate-400">
-                FullStack - PREN/MEAN - PHP/Laravel - Dart/Flutter
-            </p>
+                </h1>
+                <p className="mb-6 font-semibold lg:mb-8 text-center md:text-start text-sm sm:text-md lg:text-lg text-slate-600 dark:text-slate-400">
+                    FullStack - PREN/MEAN - PHP/Laravel - Dart/Flutter
+                </p>
+            </div>
             <GradientShineButton>{t("download_resume")}</GradientShineButton>
         </Fragment>
     );
---
+++ src/components/shared/type-writer/TypeWriter.tsx
@@ -0,0 +1,49 @@
+import { useCallback, useEffect, useState } from "react";
+
+export interface TypeWriterProps {
+    words: string[];
+    className?: string;
+}
+
+export default function TypeWriter({
+    words,
+    className
+}: TypeWriterProps): JSX.Element {
+    const typeWriterClassName = `
+        py-4 font-bold animate-cursor overflow-hidden 
+        whitespace-nowrap transition-[width] duration-200 ease-in mr-auto
+    `;
+
+    const [currentWord, setCurrentWord] = useState(0);
+    const [collapseClassName, setCollapseClassName] = useState("w-0");
+
+    const incrementWord = useCallback(() => {
+        setCollapseClassName("w-0");
+
+        setTimeout(() => {
+            setCurrentWord((prev) => (prev + 1) % words.length);
+            setCollapseClassName("w-full");
+        }, 200);
+    }, [words.length]);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(
+            () => setCollapseClassName("w-full"),
+            1000
+        );
+        const intervalId = setInterval(incrementWord, 5000);
+
+        return () => {
+            clearTimeout(timeoutId);
+            clearInterval(intervalId);
+        };
+    }, [incrementWord]);
+
+    return (
+        <div className={className}>
+            <div className={`${typeWriterClassName} ${collapseClassName}`}>
+                {words[currentWord]}
+            </div>
+        </div>
+    );
+}
