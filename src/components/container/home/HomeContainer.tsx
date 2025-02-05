7fb51f40fa8e2a5b908f06c385cbe45e0814085a
coolbeatz71 <sigmacool@gmail.com>
2025-02-05 23:26:41 +0200

    Skills: re-activate the component

--- src/components/container/home/HomeContainer.tsx
+++ src/components/container/home/HomeContainer.tsx
@@ -3,6 +3,7 @@ import Companies from "@/components/layout/companies/Companies";
 import Experiences from "@/components/layout/experiences/Experiences";
 import Hero from "@/components/layout/hero/Hero";
 import Navigation from "@/components/layout/navigation/Navigation";
+import Projects from "@/components/layout/projects/Projects";
 import BackgroundBeams from "@/components/shared/background/Beams.Background";
 import BackgroundGradient from "@/components/shared/background/Gradient.Background";
 import GridBackground from "@/components/shared/background/Grid.Background";
@@ -26,8 +27,8 @@ export default function HomeContainer(): JSX.Element {
             <AboutMe />
 
             <Experiences />
-            {/* 
             <Projects />
+            {/* 
             <Skills />
 
             <DownloadResumeFixedButton />
