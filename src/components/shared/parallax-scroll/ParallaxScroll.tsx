e98d0cf819fda4a5d5c0064b178bdac3a751a674
coolbeatz71 <sigmacool@gmail.com>
2025-02-06 00:01:55 +0200

    ParallaxScroll: use transforms 600 value for smooth scrolling: work if you add all the page elements

--- src/components/shared/parallax-scroll/ParallaxScroll.tsx
+++ src/components/shared/parallax-scroll/ParallaxScroll.tsx
@@ -30,10 +30,10 @@ export default function ParallaxScroll({
         restDelta: 0.001
     });
 
-    const translateFirst = useTransform(smoothScroll, [0, 1], [0, -64], {
+    const translateFirst = useTransform(smoothScroll, [0, 1], [0, -600], {
         ease: cubicEase
     });
-    const translateSecond = useTransform(smoothScroll, [0, 1], [0, 64], {
+    const translateSecond = useTransform(smoothScroll, [0, 1], [0, 600], {
         ease: cubicEase
     });
 
@@ -68,7 +68,7 @@ export default function ParallaxScroll({
     const imageHeight = gridHeight / divider;
 
     return (
-        <div className={cn("w-full pt-24", className)}>
+        <div className={cn("w-full pt-12", className)}>
             <div className="grid grid-cols-2 items-start max-w-5xl mx-auto gap-2 md:gap-4">
                 <div className="grid gap-2 md:gap-4">
                     {firstColumn.map((img) => (
@@ -81,7 +81,7 @@ export default function ParallaxScroll({
                             style={{ y: translateFirst }}
                         >
                             <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-indigo-400 to-indigo-900 transform scale-[0.75] rounded-full blur-2xl" />
-                            <div className="h-[14rem] md:h-[20rem] lg:h-[16rem] relative shadow-xl bg-indigo-500/[0.2] border border-gray-800/[0.1] p-1 overflow-hidden rounded-lg flex flex-col justify-end items-start">
+                            <div className="h-[14rem] md:h-[20rem] lg:h-[16rem] 2xl:h-[22rem] relative shadow-xl bg-indigo-500/[0.2] border border-gray-800/[0.1] p-1 overflow-hidden rounded-lg flex flex-col justify-end items-start">
                                 <NextImage
                                     src={img.src}
                                     alt={img.alt}
@@ -107,7 +107,7 @@ export default function ParallaxScroll({
                             style={{ y: translateSecond }}
                         >
                             <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-indigo-400 to-indigo-900 transform scale-[0.75] rounded-full blur-2xl" />
-                            <div className="h-[14rem] md:h-[20rem] lg:h-[16rem] relative shadow-xl bg-indigo-500/[0.2] border border-gray-800/[0.1] p-1 overflow-hidden rounded-lg flex flex-col justify-end items-start">
+                            <div className="h-[14rem] md:h-[20rem] lg:h-[16rem] 2xl:h-[22rem] relative shadow-xl bg-indigo-500/[0.2] border border-gray-800/[0.1] p-1 overflow-hidden rounded-lg flex flex-col justify-end items-start">
                                 <NextImage
                                     src={img.src}
                                     alt={img.alt}
