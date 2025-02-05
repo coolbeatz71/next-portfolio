70aaec41d79f5a72c56432e2ce4cad751899b155
coolbeatz71 <sigmacool@gmail.com>
2025-02-05 23:50:24 +0200

    Footer: fix contact-info and form layout on tablet

--- src/components/layout/footer/Footer.Message.Form.SubmiButton.tsx
+++ src/components/layout/footer/Footer.Message.Form.SubmiButton.tsx
@@ -14,7 +14,7 @@ export function FormSubmitButton({
         <button
             type="submit"
             disabled={isLoading}
-            className="disabled:cursor-not-allowed disabled:bg-gray-400 cursor-pointer w-full md:w-[30%] focus:outline-none text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 dark:focus:ring-indigo-800 font-medium rounded-lg text-sm p-3 lg:p-4 dark:bg-indigo-500 dark:hover:bg-indigo-700"
+            className="disabled:cursor-not-allowed disabled:bg-gray-400 cursor-pointer w-full md:w-[40%] focus:outline-none text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 dark:focus:ring-indigo-800 font-medium rounded-lg text-sm p-3 lg:p-4 dark:bg-indigo-500 dark:hover:bg-indigo-700"
         >
             {isLoading && <SpinnerIcon />}
             {t("send_message")}
