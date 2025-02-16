18d896a0f871f70388abc91b058322f55c6e56be
coolbeatz71 <sigmacool@gmail.com>
2025-02-16 18:33:52 +0200

    Hero: improve spacing on extra-large desktop

--- src/components/layout/hero/Hero.tsx
+++ src/components/layout/hero/Hero.tsx
@@ -1,8 +1,8 @@
-import Spotlight from "@/components/shared/spotlight/Spotlight";
-
 import { ScrollReveal } from "@/components/shared/scroll-reveal/ScrollReveal";
+import Spotlight from "@/components/shared/spotlight/Spotlight";
 import { RESPONSIVE_CLASSNAME } from "@/config/ThemeStyle";
 import { cn } from "@/helpers/mergeClassName";
+
 import HeroImageSection from "./hero-section/Hero.Image.Section";
 import HeroIntroSection from "./hero-section/Hero.Intro.Section";
 
@@ -17,7 +17,7 @@ export default function Hero(): JSX.Element {
                 )}
             >
                 <Spotlight />
-                <div className="grid grid-cols-1 xl:grid-cols-2 py-4 md:py-10 gap-16 md:gap-0">
+                <div className="grid grid-cols-1 xl:grid-cols-2 py-4 md:py-10 2xl:py-32 gap-16 md:gap-0">
                     <div className="flex flex-col items-start p-0 md:py-8 xl:py-16 gap-4">
                         <HeroIntroSection />
                     </div>
