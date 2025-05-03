f0cc5d5c35eedcc5a7860f2da8ee04a446b176ee
coolbeatz71 <sigmacool@gmail.com>
2025-05-03 02:01:33 +0200

    [refactor] split component into smaller

---
+++ src/components/shared/parallax-scroll/ParallaxScroll.ImageColumn.tsx
@@ -0,0 +1,59 @@
+import type { AboutMeImage } from "@/config/AboutMe.Images";
+import { cn } from "@/helpers/mergeClassName";
+import { type MotionValue, motion } from "framer-motion";
+import NextImage from "next/image";
+
+export interface ImageColumnProps {
+    columnKey: string;
+    imageHeight: number;
+    images: AboutMeImage[];
+    translate: MotionValue<number>;
+}
+
+export function ImageColumn({
+    images,
+    translate,
+    columnKey,
+    imageHeight
+}: ImageColumnProps) {
+    const gradientOverlayClassName = `absolute inset-0 h-full w-full bg-gradient-to-r from-indigo-400 
+        to-indigo-900 transform scale-[0.75] rounded-full blur-2xl`;
+
+    const imageContainerClassName = cn(
+        "h-[14rem] md:h-[20rem] lg:h-[16rem] 2xl:h-[22rem]",
+        "relative shadow-xl bg-indigo-500/[0.2] border border-gray-800/[0.1]",
+        "p-1 overflow-hidden rounded-lg flex flex-col justify-end items-start"
+    );
+
+    const imageClassName = "h-full object-cover object-left-top rounded-lg";
+
+    return (
+        <div className="grid gap-2 md:gap-4">
+            {images.map((img) => (
+                <motion.div
+                    className="relative"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    style={{ y: translate }}
+                    transition={{ duration: 1 }}
+                    key={`${columnKey}-${img.alt}`}
+                >
+                    <div className={gradientOverlayClassName} />
+                    <div className={imageContainerClassName}>
+                        <NextImage
+                            src={img.src}
+                            alt={img.alt}
+                            width={360}
+                            height={imageHeight}
+                            className={imageClassName}
+                            blurDataURL={img.blurData}
+                            placeholder="blur"
+                            loading="lazy"
+                            quality={90}
+                        />
+                    </div>
+                </motion.div>
+            ))}
+        </div>
+    );
+}
--- src/components/shared/parallax-scroll/ParallaxScroll.tsx
+++ src/components/shared/parallax-scroll/ParallaxScroll.tsx
@@ -1,15 +1,9 @@
 import type { AboutMeImage } from "@/config/AboutMe.Images";
 import { getRandomImages } from "@/helpers/getRandomImages";
 import { cn } from "@/helpers/mergeClassName";
-import {
-    cubicBezier,
-    motion,
-    useScroll,
-    useSpring,
-    useTransform
-} from "framer-motion";
-import NextImage from "next/image";
+import { cubicBezier, useScroll, useSpring, useTransform } from "framer-motion";
 import { useEffect, useMemo, useState } from "react";
+import { ImageColumn } from "./ParallaxScroll.ImageColumn";
 
 export interface ParallaxScrollProps {
     className?: string;
@@ -21,7 +15,6 @@ export function ParallaxScroll({
     className
 }: ParallaxScrollProps): JSX.Element {
     const { scrollYProgress } = useScroll();
-
     const cubicEase = cubicBezier(0.5, 0, 0.2, 1);
 
     const smoothScroll = useSpring(scrollYProgress, {
@@ -70,76 +63,18 @@ export function ParallaxScroll({
     return (
         <div className={cn("w-full pt-12", className)}>
             <div className="grid grid-cols-2 items-start max-w-5xl mx-auto gap-2 md:gap-4">
-                <div className="grid gap-2 md:gap-4">
-                    {firstColumn.map((img) => (
-                        <motion.div
-                            className="relative"
-                            key={`grid-1 ${img.alt}`}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            transition={{ duration: 1 }}
-                            style={{ y: translateFirst }}
-                        >
-                            <div
-                                className={`absolute inset-0 h-full w-full bg-gradient-to-r
-                                from-indigo-400 to-indigo-900 transform scale-[0.75] rounded-full blur-2xl`}
-                            />
-                            <div
-                                className={`h-[14rem] md:h-[20rem] lg:h-[16rem] 2xl:h-[22rem] 
-                                    relative shadow-xl bg-indigo-500/[0.2] border border-gray-800/[0.1]
-                                    p-1 overflow-hidden rounded-lg flex flex-col justify-end items-start
-                                `}
-                            >
-                                <NextImage
-                                    src={img.src}
-                                    alt={img.alt}
-                                    width={360}
-                                    height={imageHeight}
-                                    className="h-full object-cover object-left-top rounded-lg"
-                                    blurDataURL={img.blurData}
-                                    placeholder="blur"
-                                    loading="lazy"
-                                    quality={90}
-                                />
-                            </div>
-                        </motion.div>
-                    ))}
-                </div>
-                <div className="grid gap-2 md:gap-4">
-                    {secondColumn.map((img) => (
-                        <motion.div
-                            className="relative"
-                            key={`grid-2 ${img.alt}`}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            transition={{ duration: 1 }}
-                            style={{ y: translateSecond }}
-                        >
-                            <div
-                                className={`absolute inset-0 h-full w-full bg-gradient-to-r
-                                    from-indigo-400 to-indigo-900 transform scale-[0.75] rounded-full blur-2xl`}
-                            />
-                            <div
-                                className={`h-[14rem] md:h-[20rem] lg:h-[16rem] 2xl:h-[22rem]
-                                    relative shadow-xl bg-indigo-500/[0.2] border border-gray-800/[0.1]
-                                    p-1 overflow-hidden rounded-lg flex flex-col justify-end items-start
-                                `}
-                            >
-                                <NextImage
-                                    src={img.src}
-                                    alt={img.alt}
-                                    width={360}
-                                    height={imageHeight}
-                                    className="h-full object-cover object-left-top rounded-lg"
-                                    blurDataURL={img.blurData}
-                                    placeholder="blur"
-                                    loading="lazy"
-                                    quality={90}
-                                />
-                            </div>
-                        </motion.div>
-                    ))}
-                </div>
+                <ImageColumn
+                    images={firstColumn}
+                    columnKey="first-column"
+                    imageHeight={imageHeight}
+                    translate={translateFirst}
+                />
+                <ImageColumn
+                    images={secondColumn}
+                    columnKey="second-column"
+                    imageHeight={imageHeight}
+                    translate={translateSecond}
+                />
             </div>
         </div>
     );
