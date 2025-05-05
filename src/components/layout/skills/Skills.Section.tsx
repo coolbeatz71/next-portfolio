4c7dd2c7087659e39365660ea489a27ce8dba97c
coolbeatz71 <sigmacool@gmail.com>
2025-05-05 11:24:00 +0200

    refactor: remove JSX.Element return type in Skills.Section

--- src/components/layout/skills/Skills.Section.tsx
+++ src/components/layout/skills/Skills.Section.tsx
@@ -5,7 +5,7 @@ export interface SkillSectionProps {
     stacks: SkillsByStack[];
 }
 
-export function SkillSection({ stacks }: SkillSectionProps): JSX.Element {
+export function SkillSection({ stacks }: SkillSectionProps) {
     return (
         <section className="flex justify-center items-center">
             <div className="flex flex-col w-full md:px-12 lg:px-48 xl:px-72 2xl:px-96 lg:min-w-[80%] xl:min-w-[60%] 2xl:min-w-[40%] py-8">
