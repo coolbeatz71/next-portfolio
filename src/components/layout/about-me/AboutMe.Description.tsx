deebb10edab159736342bea4d8e1262edb92aabd
coolbeatz71 <sigmacool@gmail.com>
2025-05-05 12:26:00 +0200

    refactor: use v4 opacity and important modifier syntax in AboutMe.Description

--- src/components/layout/about-me/AboutMe.Description.tsx
+++ src/components/layout/about-me/AboutMe.Description.tsx
@@ -5,7 +5,7 @@ interface HighlightProps {
     children: React.ReactNode;
 }
 
-function Highlight({ children }: HighlightProps): JSX.Element {
+function Highlight({ children }: HighlightProps) {
     return (
         <span className="text-black dark:text-white font-semibold">
             {" "}
@@ -14,9 +14,9 @@ function Highlight({ children }: HighlightProps): JSX.Element {
     );
 }
 
-export function AboutMeDescription(): JSX.Element {
+export function AboutMeDescription() {
     const { t } = useTranslation();
-    const textStyle = "text-neutral-500 dark:text-neutral-400 !leading-loose";
+    const textStyle = "text-neutral-500 dark:text-neutral-400 leading-loose!";
     return (
         <Fragment>
             <h2 className="text-start mb-4 text-3xl font-bold tracking-tight md:leading-relaxed text-stone-700 dark:text-stone-200">
@@ -25,8 +25,8 @@ export function AboutMeDescription(): JSX.Element {
             <p className="text-md sm:text-lg md:text-xl lg:text-xl text-start text-neutral-500 dark:text-neutral-400 tracking-tight md:leading-relaxed">
                 {t("aboutme_subtitle")}
             </p>
-            <hr className="h-px my-4 md:my-8 border-0 bg-slate-500/[0.1] dark:bg-slate-400/[0.1]" />
-            <h2 className="text-start mb-4 text-xl md:text-xl xl:text-2xl font-bold !leading-tight text-slate-700 dark:text-slate-300">
+            <hr className="h-px my-4 md:my-8 border-0 bg-slate-500/10 dark:bg-slate-400/10" />
+            <h2 className="text-start mb-4 text-xl md:text-xl xl:text-2xl font-bold leading-tight! text-slate-700 dark:text-slate-300">
                 {t("about_myself")}
             </h2>
 
