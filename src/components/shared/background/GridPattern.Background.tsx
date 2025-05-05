f4adbba73fe4301871ff0574d5562bb346420c26
coolbeatz71 <sigmacool@gmail.com>
2025-05-05 15:54:00 +0200

    refactor: replace mask-image and bg-gradient-to-r with v4 syntax in GridPattern.Background

--- src/components/shared/background/GridPattern.Background.tsx
+++ src/components/shared/background/GridPattern.Background.tsx
@@ -5,9 +5,7 @@ export interface GridPatternBackgroundProps {
     size?: number;
 }
 
-function GridPatternBackgroundComponent({
-    size
-}: GridPatternBackgroundProps): JSX.Element {
+function GridPatternBackgroundComponent({ size }: GridPatternBackgroundProps) {
     const pattern: Array<[number, number]> = [
         [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
         [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
@@ -21,9 +19,9 @@ function GridPatternBackgroundComponent({
         [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1]
     ];
     return (
-        <div className="pointer-events-none absolute left-0 top-0 h-full w-full [mask-image:linear-gradient(white,transparent)]">
+        <div className="pointer-events-none absolute left-0 top-0 h-full w-full mask-[linear-gradient(white,transparent)]">
             <div
-                className={`absolute inset-0 z-50 bg-gradient-to-r [mask-image:radial-gradient(farthest-side_at_top,white,transparent)]
+                className={`absolute inset-0 z-50 bg-linear-to-r mask-[radial-gradient(farthest-side_at_top,white,transparent)]
                     dark:from-zinc-900/30 from-zinc-100/30 to-zinc-300/30 dark:to-zinc-900/30 opacity-100
                 `}
             >
