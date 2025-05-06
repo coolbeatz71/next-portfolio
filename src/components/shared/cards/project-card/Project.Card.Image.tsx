e76d9e3f16e223763b16abdd56f6b91178a52105
coolbeatz71 <sigmacool@gmail.com>
2025-05-06 11:18:00 +0200

    refactor: replace bg-gradient-to-t with bg-linear-to-t in Project.Card.Image

--- src/components/shared/cards/project-card/Project.Card.Image.tsx
+++ src/components/shared/cards/project-card/Project.Card.Image.tsx
@@ -31,7 +31,7 @@ export function ProjectCardImage({
             />
             <div
                 className={`
-                    absolute inset-0 bg-gradient-to-t from-black/50 to-transparent
+                    absolute inset-0 bg-linear-to-t from-black/50 to-transparent
                     opacity-0 group-hover:opacity-100 transition-opacity duration-300
                 `}
             />
