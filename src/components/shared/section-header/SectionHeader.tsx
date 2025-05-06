b70a837225daa485abc4dfbdcd43d9ec46ed40b6
coolbeatz71 <sigmacool@gmail.com>
2025-05-06 16:43:00 +0200

    refactor: use important modifier syntax and remove JSX.Element in SectionHeader

--- src/components/shared/section-header/SectionHeader.tsx
+++ src/components/shared/section-header/SectionHeader.tsx
@@ -12,7 +12,7 @@ function SectionHeaderComponent({
     title,
     subtitle,
     className
-}: SectionHeaderProps): JSX.Element {
+}: SectionHeaderProps) {
     return (
         <div className="flex flex-col items-center justify-center pb-6 md:pb-12">
             <DotBackground className="h-20 z-0" />
@@ -22,10 +22,10 @@ function SectionHeaderComponent({
                     className
                 )}
             >
-                <h2 className="mb-4 text-3xl xl:text-4xl font-bold !leading-relaxed text-stone-700 dark:text-stone-200">
+                <h2 className="mb-4 text-3xl xl:text-4xl font-bold leading-relaxed! text-stone-700 dark:text-stone-200">
                     {title}
                 </h2>
-                <p className="text-md sm:text-lg md:text-xl lg:text-xl text-neutral-500 dark:text-neutral-400 !leading-relaxed">
+                <p className="text-md sm:text-lg md:text-xl lg:text-xl text-neutral-500 dark:text-neutral-400 leading-relaxed!">
                     {subtitle}
                 </p>
             </div>
