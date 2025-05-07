658fa6f3573f71f8f1cf1fee278758a35b12c62b
coolbeatz71 <sigmacool@gmail.com>
2025-05-07 14:10:00 +0200

    refactor: remove redundant cursor-pointer from FormSubmitButton

--- src/components/layout/footer/Footer.Message.Form.SubmiButton.tsx
+++ src/components/layout/footer/Footer.Message.Form.SubmiButton.tsx
@@ -13,7 +13,7 @@ export function FormSubmitButton({ isLoading }: FormSubmitButtonProps) {
             type="submit"
             disabled={isLoading}
             className={`
-                disabled:cursor-not-allowed disabled:bg-gray-400 cursor-pointer w-full md:w-44
+                disabled:cursor-not-allowed disabled:bg-gray-400 w-full md:w-44
                 focus:outline-none text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4
                 focus:ring-indigo-300 dark:focus:ring-indigo-800 font-medium rounded-lg
                 text-sm p-3 lg:p-4 dark:bg-indigo-500 dark:hover:bg-indigo-700
