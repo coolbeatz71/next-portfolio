c531280abe1ccffc6fd3296a7edd10586e3e8ecc
coolbeatz71 <sigmacool@gmail.com>
2025-05-07 09:24:00 +0200

    refactor: remove JSX.Element return type in ThemeToggle

--- src/components/shared/theme-toggle/ThemeToggle.tsx
+++ src/components/shared/theme-toggle/ThemeToggle.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { useTheme } from "next-themes";
 import { useCallback } from "react";
 
-export function ThemeToggle(): JSX.Element {
+export function ThemeToggle() {
     const { theme, setTheme } = useTheme();
 
     const raysVariants = {
