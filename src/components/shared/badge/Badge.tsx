f924aa01408e2a83509813a94c8e587562e7fd36
coolbeatz71 <sigmacool@gmail.com>
2025-05-08 22:55:00 +0200

    refactor: sort imports in Badge

--- src/components/shared/badge/Badge.tsx
+++ src/components/shared/badge/Badge.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import numeral from "numeral";
 import type { ReactNode } from "react";
 import CountUp from "react-countup";
