5dcc010cdfd5dbe94afb774880b8d8590eb477cb
coolbeatz71 <sigmacool@gmail.com>
2025-05-11 12:31:57 +0200

    docs: add JSDoc to animation config

--- src/shared/config/animation.ts
+++ src/shared/config/animation.ts
@@ -1,9 +1,17 @@
 import type { MotionProps } from "motion/react";
 
+/**
+ * @interface AnimatedShapeProps
+ * @property {string} [className] - Additional class names for the motion element
+ */
 export interface AnimatedShapeProps extends MotionProps {
     className?: string;
 }
 
+/**
+ * @interface AnimatedLineProps
+ * @property {string} pathD - SVG `d` attribute value for the path to animate
+ */
 export interface AnimatedLineProps extends AnimatedShapeProps {
     pathD: string;
 }
