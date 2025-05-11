4cd44d813c47f001870191fa6465ee8d975d1a06
coolbeatz71 <sigmacool@gmail.com>
2025-05-11 20:45:24 +0200

    docs: add JSDoc to Popup.Header component

--- src/shared/ui/popup/Popup.Header.tsx
+++ src/shared/ui/popup/Popup.Header.tsx
@@ -6,6 +6,20 @@ export interface PopupHeaderProps {
     children: ReactNode;
 }
 
+/**
+ * Popup header component.
+ *
+ * @component
+ *
+ * @description
+ * Renders a sticky bottom-bordered container used as the header area of a modal or drawer.
+ *
+ * @param {PopupHeaderProps} props - Component props
+ * @param {ReactNode} props.children - Content to render inside the header
+ * @param {string} [props.className] - Additional class names for the header container
+ *
+ * @returns The popup header element
+ */
 function PopupHeaderComponent({ children, className }: PopupHeaderProps) {
     return (
         <div
