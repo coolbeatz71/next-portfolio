787fc9833187487a038684275334fbb09cc8c0db
coolbeatz71 <sigmacool@gmail.com>
2025-05-13 11:43:37 +0200

    docs: add JSDoc to Experiences.Item component

--- src/features/experience/ui/Experiences.Item.tsx
+++ src/features/experience/ui/Experiences.Item.tsx
@@ -4,6 +4,20 @@ export interface ExperienceItemProps {
     children: ReactNode;
 }
 
+/**
+ * Experience item component.
+ *
+ * @component
+ *
+ * @description
+ * Renders a single bullet point in an experience timeline entry,
+ * with an indigo checkmark icon followed by the item text.
+ *
+ * @param {ExperienceItemProps} props - Component props
+ * @param {ReactNode} props.children - The text content of the experience bullet point
+ *
+ * @returns The experience item element
+ */
 function ExperienceItemComponent({ children }: ExperienceItemProps) {
     return (
         <span className="flex flex-row space-x-2 my-2">
