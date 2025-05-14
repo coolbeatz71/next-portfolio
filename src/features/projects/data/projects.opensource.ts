c5b1392bc36da945c8943f7c79f5da36cdb608c3
coolbeatz71 <sigmacool@gmail.com>
2025-05-14 08:47:52 +0200

    refactor: update ProjectByStack import to IProjectByStack

--- src/features/projects/data/projects.opensource.ts
+++ src/features/projects/data/projects.opensource.ts
@@ -1,6 +1,6 @@
-import type { ProjectByStack } from "@/features/projects/data/projects.tabs";
+import type { IProjectByStack } from "@/features/projects/data/projects.tabs";
 
-export const projectsOpenSource: ProjectByStack[] = [
+export const projectsOpenSource: IProjectByStack[] = [
     {
         name: "Coolest Dark",
         role: "software_engineer",
