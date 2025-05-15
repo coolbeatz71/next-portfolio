1881d907f3b552414783f857145b82a6b5e25991
coolbeatz71 <sigmacool@gmail.com>
2025-05-15 23:38:30 +0200

    refactor: extract static beam path and gradient data to dedicated file

---
+++ src/shared/ui/background/Beams.Background.Data.ts
@@ -0,0 +1,24 @@
+import type { Transition } from "motion/react";
+
+export const beamPaths = [
+    "M-156 -445C-156 -445 -88 -40 376 87C840 214 908 619 908 619",
+    "M-149 -453C-149 -453 -81 -48 383 79C847 206 915 611 915 611",
+    "M-142 -461C-142 -461 -74 -56 390 71C854 198 922 603 922 603",
+    "M-135 -469C-135 -469 -67 -64 397 63C861 190 929 595 929 595",
+    "M-128 -477C-128 -477 -60 -72 404 55C868 182 936 587 936 587",
+    "M-121 -485C-121 -485 -53 -80 411 47C875 174 943 579 943 579",
+    "M-114 -493C-114 -493 -46 -88 418 39C882 166 950 571 950 571",
+    "M-107 -501C-107 -501 -39 -96 425 31C889 158 957 563 957 563",
+    "M-100 -509C-100 -509 -32 -104 432 23C896 150 964 555 964 555"
+];
+
+export const beamGradients: { transition: Transition; y2End: string }[] =
+    beamPaths.map(() => ({
+        transition: {
+            duration: Math.random() * 10 + 10,
+            ease: "easeInOut" as const,
+            repeat: Number.POSITIVE_INFINITY,
+            delay: Math.random() * 5
+        },
+        y2End: `${93 + Math.random() * 8}%`
+    }));
