10e74ccc83bca2959e33f55169ae0c26245d1105
coolbeatz71 <sigmacool@gmail.com>
2025-05-15 15:05:30 +0200

    refactor: move HighlightProps interface to types file

---
+++ src/features/about/ui/types.ts
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+/**
+ * @interface HighlightProps
+ * @property {ReactNode} children - Content to render inside the highlight span
+ */
+export interface HighlightProps {
+    children: ReactNode;
+}
