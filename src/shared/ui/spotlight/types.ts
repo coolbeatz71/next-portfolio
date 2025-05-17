2499e6806d2b06d5c9f7a04928fb101cbe0221af
coolbeatz71 <sigmacool@gmail.com>
2025-05-17 14:23:30 +0200

    refactor: move SpotlightProps to types file

---
+++ src/shared/ui/spotlight/types.ts
@@ -0,0 +1,9 @@
+/**
+ * @interface SpotlightProps
+ * @property {string} [fill] - Fill color for the ellipse; defaults to "#ca38b7"
+ * @property {string} [className] - Additional class names for sizing and positioning
+ */
+export interface SpotlightProps {
+    fill?: string;
+    className?: string;
+}
