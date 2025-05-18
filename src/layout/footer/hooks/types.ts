794c5e6091d16cd0b9e314e88257ffcaed75eee2
coolbeatz71 <sigmacool@gmail.com>
2025-05-18 08:41:30 +0200

    refactor: add useFooterMessageForm hook for form and EmailJS logic

---
+++ src/layout/footer/hooks/types.ts
@@ -0,0 +1,11 @@
+/**
+ * @interface IMessageFormData
+ * @property {string} name - Sender's full name
+ * @property {string} email - Sender's email address
+ * @property {string} message - Message body text
+ */
+export interface IMessageFormData {
+    name: string;
+    email: string;
+    message: string;
+}
