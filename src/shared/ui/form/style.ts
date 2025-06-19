c7eb4c42f75be1291de24cbb4f13725698491fd1
coolbeatz71 <sigmacool@gmail.com>
2025-06-19 16:38:00 +0200

    chore: apply outline and focus token classes in form style constants

--- src/shared/ui/form/style.ts
+++ src/shared/ui/form/style.ts
@@ -1,14 +1,14 @@
 export const BASE_INPUT_CLASSNAME = `peer block border rounded-lg px-4 pb-2 pt-6 w-full
 resize-none focus:outline-none focus:ring-4 bg-surface-input`;
 
-export const ERROR_INPUT_CLASSNAME = "border-error focus:ring-ring-error";
+export const ERROR_INPUT_CLASSNAME = "border-error focus:ring-focus-error";
 
-export const NORMAL_INPUT_CLASSNAME = `border-border-input focus:border-primary
-focus:ring-ring-primary`;
+export const NORMAL_INPUT_CLASSNAME = `border-outline-input focus:border-primary
+focus:ring-focus-primary`;
 
 export const BASE_LABEL_CLASSNAME = `absolute left-4 top-4 origin-[0] -translate-y-3 scale-75 duration-fast
 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-90 peer-focus:-translate-y-3
 peer-focus:scale-75 first-letter:uppercase`;
 
 export const ERROR_CLASSNAME = "text-error-text";
-export const NORMAL_LABEL_CLASSNAME = "text-text-muted";
+export const NORMAL_LABEL_CLASSNAME = "text-typography-muted";
