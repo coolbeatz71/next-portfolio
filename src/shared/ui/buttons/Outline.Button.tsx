d1356d9c71113e7b4b75e99c7b04f5723415f212
coolbeatz71 <sigmacool@gmail.com>
2026-02-22 04:39:08 +0200

    fix(outline-button): update animation class reference to outlined

--- src/shared/ui/buttons/Outline.Button.tsx
+++ src/shared/ui/buttons/Outline.Button.tsx
@@ -38,7 +38,7 @@ export function OutlineButton({
                 borderRadius: borderRadius
             }}
             className={cn(
-                "inline-flex animate-Outline items-center justify-center border-4 border-surface-hover px-6 font-medium text-typography-nav focus:outline-none focus:ring-2 focus:ring-focus-surface",
+                "inline-flex animate-outlined items-center justify-center border-4 border-surface-hover px-6 font-medium text-typography-nav focus:outline-none focus:ring-2 focus:ring-focus-surface",
                 className
             )}
             {...otherProps}
