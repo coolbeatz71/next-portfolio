8772349f244c7c9be4d748b017fc9e12c20890dc
coolbeatz71 <sigmacool@gmail.com>
2026-02-24 08:55:56 +0200

    fix: replace useMemo with useEffect and useRef in I18n provider

--- src/app/providers/I18n.Provider.tsx
+++ src/app/providers/I18n.Provider.tsx
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { useMemo } from "react";
+import { useEffect, useRef } from "react";
 import locales from "@/shared/i18n";
 import { getLanguage } from "@/shared/lib/getLanguage";
 import type { I18nProviderProps } from "./types";
@@ -21,17 +21,17 @@ import type { I18nProviderProps } from "./types";
  * @returns The i18n-initialized children element
  */
 export function I18nProvider({ children }: I18nProviderProps) {
-    const userLang = getLanguage();
+    const userLang = useRef(getLanguage());
 
-    useMemo(() => {
+    useEffect(() => {
         const initLanguage = (lang: string): void => {
             locales.changeLanguage(lang);
             dayjs.locale(lang);
         };
 
         dayjs.extend(relativeTime);
-        initLanguage(userLang as string);
-    }, [userLang]);
+        initLanguage(userLang.current as string);
+    }, []);
 
     return <>{children}</>;
 }
