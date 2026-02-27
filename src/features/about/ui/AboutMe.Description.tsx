0a13d400a074e761c9c222a2b7b93bed9606dbc6
coolbeatz71 <sigmacool@gmail.com>
2026-02-27 21:47:27 +0200

    style: apply linting and formatting fixes

--- src/features/about/ui/AboutMe.Description.tsx
+++ src/features/about/ui/AboutMe.Description.tsx
@@ -3,12 +3,7 @@ import { useTranslation } from "react-i18next";
 import type { HighlightProps } from "./types";
 
 function Highlight({ children }: HighlightProps) {
-    return (
-        <span className="text-typography-inverse font-semibold">
-            {" "}
-            {children}
-        </span>
-    );
+    return <span className="text-typography-inverse font-semibold"> {children}</span>;
 }
 
 /**
@@ -42,23 +37,18 @@ export const AboutMeDescription = memo(function AboutMeDescription() {
 
             <p className={`text-start ${textStyle} mb-4`}>
                 {t("about_myself_content", { name: "Jean-Vincent" })}
-                <Highlight>JavaScript</Highlight>,
-                <Highlight>HTML/CSS</Highlight>,<Highlight>PHP</Highlight>,
-                <Highlight>C#</Highlight>, {t("about_myself_content_2")}
+                <Highlight>JavaScript</Highlight>,<Highlight>HTML/CSS</Highlight>,
+                <Highlight>PHP</Highlight>,<Highlight>C#</Highlight>, {t("about_myself_content_2")}
                 <Highlight>ReactJS</Highlight>,<Highlight>NodeJS</Highlight>,
-                <Highlight>Typescript</Highlight>,<Highlight>Angular</Highlight>
-                ,<Highlight>Laravel</Highlight>,<Highlight>.NET</Highlight>,{" "}
-                {t("and")} <Highlight>Dart/Flutter </Highlight>
+                <Highlight>Typescript</Highlight>,<Highlight>Angular</Highlight>,
+                <Highlight>Laravel</Highlight>,<Highlight>.NET</Highlight>, {t("and")}{" "}
+                <Highlight>Dart/Flutter </Highlight>
                 {t("about_myself_content_3")}
             </p>
 
-            <p className={`text-start ${textStyle} mb-4`}>
-                {t("about_myself_content_4")}
-            </p>
+            <p className={`text-start ${textStyle} mb-4`}>{t("about_myself_content_4")}</p>
 
-            <p className={`text-start ${textStyle}`}>
-                {t("about_myself_content_5")}
-            </p>
+            <p className={`text-start ${textStyle}`}>{t("about_myself_content_5")}</p>
         </Fragment>
     );
 });
