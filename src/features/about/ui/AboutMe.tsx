02b392097a07cf742e795aad93f8a3694f771c3f
coolbeatz71 <sigmacool@gmail.com>
2026-02-27 21:57:54 +0200

    style: apply linting and formatting fixes

--- src/features/about/ui/AboutMe.tsx
+++ src/features/about/ui/AboutMe.tsx
@@ -18,13 +18,7 @@ import { AboutMeImages } from "./AboutMe.Images";
 export function AboutMe() {
     return (
         <ScrollReveal direction="left" className="delay-300">
-            <section
-                id="about"
-                className={cn(
-                    RESPONSIVE_CLASSNAME,
-                    "py-12 xl:py-32 scroll-mt-20"
-                )}
-            >
+            <section id="about" className={cn(RESPONSIVE_CLASSNAME, "py-12 xl:py-32 scroll-mt-20")}>
                 <div className="grid grid-cols-1 lg:grid-cols-[2.5fr_3fr] xl:grid-cols-[3fr_3.5fr] gap-4 md:gap-12 lg:gap-8 xl:gap-12">
                     <div className="flex flex-col w-full">
                         <AboutMeImages />
